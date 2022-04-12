b3abc8a61b96aa374651d336b2c0396ebfc44ce0
jakobveselsky <jveselsky@luc.edu>
2022-04-11 22:53:26 -0500

    addition of resume details

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('home.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(debug=True)
