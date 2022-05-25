facf0e37ac757c7c1fb7ce37fd74982369e377c8
jakobveselsky <jveselsky@luc.edu>
2022-05-24 23:48:45 -0500

    ohh it looks so much better

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
